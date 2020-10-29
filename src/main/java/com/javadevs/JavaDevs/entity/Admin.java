78ff52374b088768808ecd0c24b3ca3df3d54b78
Leonardo Rodrigues <leonardo.rodrigues1994@hotmail.com>
2020-10-28 21:32:49 -0300

    Criado Entities User, Admin e Appoitment

---
+++ src/main/java/com/javadevs/JavaDevs/entity/Admin.java
@@ -0,0 +1,13 @@
+package com.javadevs.JavaDevs.entity;
+
+public class Admin {
+    private double maxAmount;
+
+    public double getMaxAmount() {
+        return maxAmount;
+    }
+
+    public void setMaxAmount(double maxAmount) {
+        this.maxAmount = maxAmount;
+    }
+}
---
+++ src/main/java/com/javadevs/JavaDevs/entity/Appoitment.java
@@ -0,0 +1,51 @@
+package com.javadevs.JavaDevs.entity;
+
+import java.util.Date;
+
+public class Appoitment {
+    private int id;
+    private boolean status;
+    private Date date;
+    private double amount;
+    private Actor actor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Actor getActor() {
+        return actor;
+    }
+
+    public void setActor(Actor actor) {
+        this.actor = actor;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
