1296532fb811a6672b8dedc49fe52d39001e314f
Carlos-MCV <66149594+Carlos-MCV@users.noreply.github.com>
2020-10-28 22:57:35 -0300

    Update ActorController.java

--- src/main/java/com/javadevs/JavaDevs/controller/ActorController.java
+++ src/main/java/com/javadevs/JavaDevs/controller/ActorController.java
@@ -1,9 +1,38 @@
 package com.javadevs.JavaDevs.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/actor")
 public class ActorController {
+
+    @Autowired
+    private ActorController service;
+
+    @GetMapping("/{actorId}")
+    public ResponseEntity<Actor> getActorById(@PathVariable int actorId) {
+        return ResponseEntity.ok(service.getActorById(actorId));
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Actor> createnewActor(@RequestBody Actor actor) {
+        Actor savedActor = service.saveActor(actor);
+
+        return ResponseEntity.ok(savedActor);
+    }
+
+    @PutMapping("/{Actor}")
+    public ResponseEntity<Actor> updateActor(@PathVariable int ActorId, @RequestBody TodoList todoList) {
+        return ResponseEntity.ok(service.putActor(actortId, actor));
+    }
+
+    @DeleteMapping("/{ActorId}")
+    public ResponseEntity<int> deleteActor(@PathVariable int actorId) {
+        service.deleteActor(actorId);
+        return new ResponseEntity<>("", HttpStatus.OK);
+    }
 }
