e461d3af5f00159fdc5a27184b002c615b032fc3
Jefferson Santos <yuiti.santos@gmail.com>
2020-10-29 18:52:27 -0300

    [update: @Controller to @RestController]

--- src/main/java/com/javadevs/JavaDevs/controller/ActorController.java
+++ src/main/java/com/javadevs/JavaDevs/controller/ActorController.java
@@ -1,9 +1,9 @@
 package com.javadevs.JavaDevs.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/actor")
 public class ActorController {
 
