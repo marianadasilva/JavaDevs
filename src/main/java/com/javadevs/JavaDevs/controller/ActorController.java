ddb2e349d1b6431a89b893e0a02c1bf07e2eaf49
Jefferson Santos <yuiti.santos@gmail.com>
2020-10-29 19:36:50 -0300

    Resolve merge conflict

--- src/main/java/com/javadevs/JavaDevs/controller/ActorController.java
+++ src/main/java/com/javadevs/JavaDevs/controller/ActorController.java
@@ -1,17 +1,15 @@
 package com.javadevs.JavaDevs.controller;
 
-import com.javadevs.JavaDevs.JavaDevsApplication;
 import com.javadevs.JavaDevs.entity.ActorEntity;
 import com.javadevs.JavaDevs.service.ActorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/actor")
 public class ActorController {
     @Autowired
