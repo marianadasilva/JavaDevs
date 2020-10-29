25567b8cdd4322a29163f9691dd77f868430464d
Carlos-MCV <66149594+Carlos-MCV@users.noreply.github.com>
2020-10-29 01:02:38 -0300

    Update ActorRepository.java

--- src/main/java/com/javadevs/JavaDevs/repository/ActorRepository.java
+++ src/main/java/com/javadevs/JavaDevs/repository/ActorRepository.java
@@ -2,7 +2,9 @@ package com.javadevs.JavaDevs.repository;
 
 import com.javadevs.JavaDevs.entity.ActorEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface ActorRepository extends CrudRepository<ActorEntity, Integer>  {
 
 }
